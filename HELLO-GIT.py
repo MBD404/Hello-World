e03518facd7f29d48a30f1875995be28ec0317ec
Miguel Bento <rickhentailove3@gmail.com>
2024-10-01 09:36:16 -0300

    Create HELLO-GIT.py

---
+++ HELLO-GIT.py
@@ -0,0 +1,15 @@
+# Hello, this is a simple code, just to say “Hello World” to you...
+#Of course, it's easier to create a .txt file and paste a big ASCII “Hello World”
+#But I decided to do this :)
+
+print("""
+    __  __________    __    ____     _       ______  ____  __    ____ 
+   / / / / ____/ /   / /   / __ \   | |     / / __ \/ __ \/ /   / __ \\
+  / /_/ / __/ / /   / /   / / / /   | | /| / / / / / /_/ / /   / / / /
+ / __  / /___/ /___/ /___/ /_/ /    | |/ |/ / /_/ / _, _/ /___/ /_/ / 
+/_/ /_/_____/_____/_____/\____/     |__/|__/\____/_/ |_/_____/_____/  
+                                                                      
+
+""")
+
+#  I'll be honest, this font is rad...
